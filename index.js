50b72d2c89090afaf07349f62aa55bf06d3a40d6
asjsit <saroenkoanton0@gmail.com>
2023-05-21 10:48:46 +0600

    update

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
-const axios = require('axios')
+const { post } = require('axios')
 const url = 'https://kcbot.ru/server/api/'
 
 exports.KingCoinApi = class {
     constructor({ token } = {}) {
         this.token = token 
     }
-    getMyBalance(action = 'myBalance', token = this.token) {
-        return axios.post(url, {
-            action,
-            token
-        }).then(data => data?.data).catch(console.error)
+    callApi(data = {}) {
+        return post(url, data).then(data => data?.data).catch((err) => err)
+    }
+
+    getMyBalance({ action = 'myBalance', token = this.token } = {}) {
+        return this.callApi({ action, token })
     }
 
     getUserBalance({ action = 'balance', token = this.token, user_ids = this.user_ids } = {}) {
-        return axios.post(url, {
-            action,
-            token,
-            user_ids
-        }).then(data => data?.data).catch(console.error)
+        return this.callApi({ action, token, user_ids })
     }
 
     getHistory({ action = 'tx', token = this.token, filter = this.filter, count = this.count, offset = this.offset } = {}) {
-        return axios.post(url, {
+        return this.callApi({ 
             action,
             token,
             filter: !filter ? 'ALL' : filter,
-            count: !count ? 100 : count,
+            count: !count ? 20 : count,
             offset: !offset ? 0 : offset
-        }).then(data => data?.data).catch(console.error)
+        })
     }
     
     sendCoins({ action = 'transfer', token = this.token, receiver = this.receiver, amount = this.amount, payload = this.payload } = {}) {
-        return axios.post(url, {
+        return this.callApi({
             action,
             token,
             receiver,
             amount,
             payload: !payload ? 0 : payload
-        }).then(data => data?.data).catch(console.error)
+        })
     }
 }
